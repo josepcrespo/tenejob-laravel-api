ec9d0737edde6777617b4e806aed0db7c2394331
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2018-12-29 20:24:58 +0000

    Bugfix

--- resources/views/days/table.blade.php
+++ resources/views/days/table.blade.php
@@ -12,7 +12,7 @@
             <td>
                 {!! Form::open(['route' => ['days.destroy', $day->id], 'method' => 'delete']) !!}
                 <div class='btn-group'>
-                    <a href="{!! route('days.show', [$day->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open">&nbsp;View</i></a>
+                    <a href="{!! route('days.show', [$day->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i>&nbsp;View</a>
                     <a href="{!! route('days.edit', [$day->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i>&nbsp;Edit</a>
                     {!! Form::button('<i class="glyphicon glyphicon-trash"></i>&nbsp;Delete', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
                 </div>
