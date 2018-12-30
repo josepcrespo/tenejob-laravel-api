232184efbb8de5e204ef980fd5066b117ed1ac4d
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2018-12-30 19:58:06 +0000

    This exception should also being set in the APIRequest as well.

--- app/Http/Requests/API/UpdateDayAPIRequest.php
+++ app/Http/Requests/API/UpdateDayAPIRequest.php
@@ -24,6 +24,21 @@ class UpdateDayAPIRequest extends APIRequest
      */
     public function rules()
     {
-        return Day::$rules;
+        $dayId         = $this->day;
+        $requestMethod = $this->method();
+        $rules         = Day::$rules;
+
+        switch ($requestMethod) {
+            case 'PATCH':
+            case 'PUT':
+                // Adding Additional Where Clauses
+                // You may also specify more conditions that will be added as "where" clauses to the query:
+                // 'name' => 'required|string|unique:days'
+                // In this rule, only rows with "id" equal to the $userId would be included in the unique check.
+                $rules['name'] = $rules['name'] . ',id,' . $dayId;
+                break;
+        }
+
+        return $rules;
     }
 }
