2c8ebcf20e0e2a2a040623eb5f1fe7b72a5e5ca6
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2019-01-04 03:25:57 +0000

    Short text describing the tools on the admin and, links to them.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,12 @@
 @section('content')
 <div class="container">
     <div class="row">
-
-
+    	<div class="col-md-12">
+	        <div style="align-items: center; display: flex; justify-content: center; height: 50vh;">
+	        	<p>Create, view and, edit&nbsp;<a href="{{ url('/') }}/days">Days</a>,&nbsp;<a href="{{ url('/') }}/shifts">Shifts</a>,&nbsp;<a href="{{ url('/') }}/workers">Workers</a>&nbsp;and,&nbsp;<a href="{{ url('/') }}/matchings">Matchings</a>
+	        	</p>
+	        </div>
+	    </div>
     </div>
 </div>
 @endsection
