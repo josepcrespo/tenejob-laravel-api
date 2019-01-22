e6df455413b5680c9bf5ef252a88210ffa7c97bc
Josep Crespo Santacreu <josep.crespo.santacreu@gmail.com>
2019-01-22 18:44:04 +0000

    Links to "/home" have been renamed to "Admin Panel" on the UI.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -137,7 +137,7 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{!! url('/home') !!}">Home</a></li>
+                    <li><a href="{!! url('/home') !!}">Admin Panel</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,7 +90,7 @@
                 <div class="links">
                     @if (Route::has('login'))
                         @auth
-                            <a href="{{ url('/home') }}">Home</a>
+                            <a href="{{ url('/home') }}">Admin Panel</a>
                         @else
                             <a href="{{ route('login') }}">Login</a>
 
